f066dc28e32fe9889f60ecdf379fb2eb43d397e0
Fredson Menezes Sumi Barreto <fredson.sumi@ufba.br>
2024-08-31 16:27:05 -0300

    Adicionado tratamento de deadline perdida

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import plotly.figure_factory as ff
 from dataclasses import dataclass
-import queue
 import argparse
+from typing import Generator, Optional
+
 
 
 @dataclass
@@ -73,6 +74,16 @@ class Task:
             priority_function: Função que calcula a prioridade da tarefa.
         """
         self.priority_function = priority_function
+    
+    def __hash__(self) -> int:
+        return hash((self.name, self.period, self.cost))
+    
+
+    def __lt__(self, other) -> bool:
+        """
+        Compara pela prioridade, depois pelo período, custo e nome.
+        """
+        return (self.priority, self.period, self.cost, self.name) < (other.priority, other.period, other.cost, other.name)
 
 
 class TaskQueue:
@@ -80,10 +91,10 @@ class TaskQueue:
     Classe que gerencia uma fila de tarefas usando o algoritmo de deadline.
     """
     def __init__(self) -> None:
-        self.queue = []
+        self.queue = set()
 
     def put(self, task: Task) -> None:
-        self.queue.append(task)
+        self.queue.add(task)
         
     def get(self) -> Task:
         """
@@ -94,7 +105,7 @@ class TaskQueue:
         """
         if not self.queue:
             return None
-        task = min(self.queue, key=lambda x: x.priority)
+        task = min(self.queue, key=lambda x: x)
         self.queue.remove(task)
         return task
     
@@ -110,20 +121,21 @@ class TaskQueue:
         """
         return min(self.queue, key=lambda x: x.priority)
     
-    def check_deadlines(self, time: int) -> Task:
+    def yield_lost_deadlines(self, time: int) -> Generator[Task, None, None]:
         """
         Verifica se há alguma tarefa que perdeu seu deadline.
 
         Args:
             time (int): Tempo atual do sistema.
 
-        Returns:
-            Task: A tarefa que perdeu o deadline, se houver.
+        Yields:
+            Task: Tarefa que perderam o deadline.
         """
         for task in self.queue:
-            if task.deadline < time:
-                return task
-        return None
+            if task.deadline <= time:
+                yield task
+
+                
 
 
 def from_args(args, tasks):
@@ -179,6 +191,12 @@ simulated_time, system_tick, q = from_args(args, tasks)
 time = 0
 current_task = None
 df = pd.DataFrame([])
+missed_tasks = []
+
+# Adiciona tasks em tempo 0,0 só para aparecer no gráfico
+for task in tasks:
+    df = pd.concat([df, pd.DataFrame({'Task': [task.name], 'Start': [0], 'Finish': [0]})])
+
 while time < simulated_time:
     # Simula o tempo
     if current_task:
@@ -189,6 +207,13 @@ while time < simulated_time:
         current_task.end_task(time)
         df = pd.concat([df, pd.DataFrame({'Task': [current_task.name], 'Start': [current_task.started_at], 'Finish': [time]})])
         current_task = None
+
+     # Verifica se alguma tarefa perdeu o deadline
+    if current_task and current_task.deadline <= time:
+        missed_tasks.append((current_task, time))
+
+    for missed_task in q.yield_lost_deadlines(time):
+        missed_tasks.append((missed_task, time))
     
     # Inicia as tarefas em seu período
     for task in tasks:
@@ -204,7 +229,8 @@ while time < simulated_time:
     # Verifica se há uma tarefa de maior prioridade
     if not q.empty():
         next_task = q.peek()
-        if next_task.priority < current_task.priority:
+        if next_task < current_task:
+            print(f"{next_task} < {current_task}")
             current_task.interrupt_task(time, system_tick)
             q.put(current_task) # Coloca a tarefa atual de volta na fila
             df = pd.concat([df, pd.DataFrame({'Task': [current_task.name], 'Start': [current_task.started_at], 'Finish': [time]})])
@@ -212,11 +238,8 @@ while time < simulated_time:
             current_task.run_task(time)
             q.get()
 
-    # Verifica se alguma tarefa perdeu o deadline
-    missed_task = q.check_deadlines(time)
 
-    if missed_task:
-        print(f"Task {missed_task.name} missed its deadline at {time}")
+
     time += system_tick
 
 # Se ainda há uma tarefa, finaliza e adiciona ao dataframe
@@ -236,6 +259,11 @@ colors = {task.name: color_rotation[i % len(color_rotation)] for i, task in enum
 fig = ff.create_gantt(df, bar_width = 0.4, show_colorbar=True,  group_tasks=True, index_col='Task', showgrid_x=True,
                       colors=colors)                       
 subtitle = "<b>Tasks:</b> " + ", ".join([f"<span style='color:{colors[task.name]}'>{task.name}({task.cost}, {task.period})</span>" for task in tasks])
+
+# Adiciona anotação, explicando que triangulos representam deadlines perdidos
+# Com desenho de um triângulo
+
+
 
 if args.algorithm == "rm":
     title = "Rate Monotonic"
@@ -272,6 +300,15 @@ fig.update_layout(
     )
 )
 
+fig.add_annotation(
+    xref='paper', yref='paper',
+    x=0.5, y=-0.25,
+    text='<b>▼:</b> Deadline Perdido',
+    showarrow=False,
+    font=dict(size=12),
+    align='left'
+)
+
 # Adiciona linhas verticais no gráfico para indicar os períodos de cada tarefa:
 for i, task in enumerate(tasks):
     for period in range(0, simulated_time, task.period):
@@ -285,6 +322,20 @@ for i, task in enumerate(tasks):
             yref='paper',
             line=dict(color=color_rotation[i % len(color_rotation)], width=1, dash="dash")
         )
+# Adiciona marcadores triangulares nos deadlines perdidos
+for missed_task, time in missed_tasks:
+    deadline_time = time
+
+    print(f"Tarefa {missed_task.name} perdeu o deadline em {time}")
+    # Calcula a posição do triângulo baseado na tarefa que falhou
+    task_position = next(((len(tasks) - i -0.75) for i, task in enumerate(tasks) if task.name == missed_task.name), 0)  # Aqui você pode ajustar a posição com base na tarefa
+
+    fig.add_shape(
+        type="path",
+        path=f'M {deadline_time-0.2}, {task_position-0.2} L {deadline_time+0.2}, {task_position-0.2} L {deadline_time}, {task_position-0.4} Z',
+        fillcolor=colors[missed_task.name],
+        line=dict(color='black', width=1),
+    )
 
 # Save the figure png
 fig.show()
